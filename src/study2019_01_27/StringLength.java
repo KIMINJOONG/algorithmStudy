0418a77eb3fdff4b47acaaa93fa5b3493e0f9ce9
KIMINJOONG <ijboym216@gmail.com>
2019-02-06 18:15:39 +0900

    재귀함수로 미로찾기

--- src/study2019_01_27/StringLength.java
+++ src/study2019_01_27/StringLength.java
@@ -1,5 +1,7 @@
 package study2019_01_27;
 
+import java.util.Scanner;
+
 public class StringLength {
 	// 문자열의 길이구하는 재귀함수
 	public static int length(String str) {
@@ -38,20 +40,113 @@ public class StringLength {
 		}
 	}
 	
+	// 0번째 배열에서 n번째 배열의 합
 	public static int sum(int n, int[] data) {
 		if(n<=0)
 			return 0;
 		else
 			return sum(n-1, data) + data[n-1];
 	}
+	
+	public static void readFrom(int n, int[] data, Scanner in) {
+		if(n==0)
+			return;
+		else {
+			readFrom(n-1, data, in);
+			data[n-1] = in.nextInt();
+		}
+	}
+	
+	// 순차탐색
+	// 검색할 구간의 끝위치는 명시적으로 존재
+	// 시작위치는 0으로 암시적위치
+	public int search1(int[] data, int n, int target) {
+		for(int i=0; i<n; i++)
+			if(data[i]==target)
+				return i;
+		return -1;
+	}
+	
+	// 매개변수의 명시화: 순차 검색
+	// 검색할 구간의 시작 위치 끝위치 명시적으로 존재
+	// 재귀함수를 작성할때 가장 기본적인 원칙중 하나
+	
+	public int search(int[] data, int begin, int end, int target) {
+		if(begin > end)
+			return -1;
+		else if(target==data[begin])
+			return begin;
+		else
+			return search(data, begin+1, end, target);
+	}
+	
+	public int search2(int [] data, int begin, int end, int target) {
+		if(begin>end)
+			return -1;
+		else if(target==data[end])
+			return end;
+		else
+			return search(data, begin, end-1, target);
+	}
+	
+	public int search3(int[] data, int begin, int end, int target) {
+		if(begin > end)
+			return -1;
+		else {
+			int middle = (begin+end) /2;
+			if(data[middle]==target)
+				return middle;
+			int index = search(data, begin, middle-1, target);
+			if(index != -1)
+				return index;
+			else
+				return search(data, middle+1, end, target);
+		}
+	}
+	//최대값 찾기
+	public int findMax(int [] data, int begin, int end) {
+		if(begin == end)
+			return data[begin];
+		else
+			return Math.max(data[begin], findMax(data, begin+1, end));
+	}
+	
+	public int findMax2(int[] data, int begin, int end) {
+		if(begin == end)
+			return data[begin];
+		else {
+			int middle = (begin + end) /2;
+			int max1 = findMax2(data, begin, middle);
+			int max2 = findMax2(data, middle+1, end);
+			return Math.max(max1, max2);
+		}
+	}
+	
+	public static int binarySearch2(String[] items, String target, int begin, int end) {
+		if(begin>end)
+			return -1;
+		else {
+			int middle = (begin + end) /2;
+			int compResult = target.compareTo(items[middle]);
+			if(compResult == 0)
+				return middle;
+			else if(compResult < 0)
+				return binarySearch2(items, target, begin, middle-1);
+			else
+				return binarySearch2(items, target, middle+1, end);
+		}
+	}
+	
 	public static void main (String[] args) {
 //		int result = length("abc");
 //		System.out.println(result);
 //		printChars("kiminjoong");
 //		printCharsReverse("kiminjoong");
 //		printInBinary(13);
-		int a[] = {1,2,3,4};
-		int result = sum(4, a);
-		System.out.println(result);
+//		int a[] = {1,2,3,4};
+//		int result = sum(4, a);
+//		System.out.println(result);
+		
+		
 	}
 }
---
+++ src/study2019_02_06/maze.java
@@ -0,0 +1,43 @@
+package study2019_02_06;
+
+public class maze {
+	private static int N = 8;
+	private static int [][] maze = {
+			{0, 0, 0, 0, 0, 0, 0, 1},
+			{0, 1, 1, 0, 1, 1, 0, 1},
+			{0, 0, 0, 1, 0, 0, 0, 1},
+			{0, 1, 0, 0, 1, 1, 0, 0},
+			{0, 1, 1, 1, 0, 0, 1, 1},
+			{0, 1, 0, 0, 0, 1, 0, 1},
+			{0, 1, 1, 1, 0, 1, 0, 0}
+	};
+	
+	private static final int PATHWAY_COLOUR = 0;
+	private static final int WALL_COLOUR = 1;
+	private static final int BLOCKED_COLOUR = 2;
+	private static final int PATH_COLOUR = 3;
+	
+	public static boolean findMazePath(int x, int y) {
+		if(x<0 || y<0 || x >= N || y >= N)
+			return false;
+		else if(maze[x][y] != PATHWAY_COLOUR)
+			return false;
+		else if(x==N-1 && y == N-1) {
+			maze[x][y] = PATH_COLOUR;
+			return true;
+		} else {
+			maze[x][y] = PATH_COLOUR;
+			if(findMazePath(x-1, y) || findMazePath(x, y+1) || findMazePath(x+1, y) || findMazePath(x, y-1)) {
+				return true;
+			}
+			maze[x][y] = BLOCKED_COLOUR;
+			return false;
+		}
+	}
+	
+	public static void main(String[] args) {
+		printMaze();
+		findMazePath(0, 0);
+		printMaze();
+	}
+}
